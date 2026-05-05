cc5b1ab62b9f8d162f50f89e5361a049ea5afb63
NeoZid <zidereckn06@gmail.com>
2026-05-04 20:30:06 -0400

    changed the backup script to satisfy cron schedule, it backs up every hour now!

--- backup.sh
+++ backup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-sleep 20
-pg_dump -h db -U $DB_USER -d $DB_NAME -f /backups/backup_$(date +%Y%m%d_%H%M%S).sql
-echo "Backup complete"
+
+apt-get update -qq && apt-get install -y cron
+
+echo "0 * * * * pg_dump -h db -U $DB_USER -d $DB_NAME -f /backups/backup_\$(date +\%Y\%m\%d_\%H\%M\%S).sql >> /var/log/backup.log 2>&1" > /etc/cron.d/backup
+
+chmod 0644 /etc/cron.d/backup
+
+cron -f
